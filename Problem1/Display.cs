01ce70046292d8ca4ca847616c8b5d00550f5077
Mohammad <mohammad.kh1742000@gmail.com>
2025-04-22 08:34:51 +0300

    Add problem1 project

---
+++ Problem1/Display.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course20.TermostateProject
+{
+    public partial class Display : Form
+    {
+        public Display()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+            Subscribe(thermostate1);
+        }
+
+        private void thermostate1_OnTemperatureChanged(object sender, Thermostate.ChangeTemperatureEventArgs e)
+        {
+            MessageBox.Show($"OldTemp : {e.OldTemp}\nNewTemp : {e.NewTemp}\nDiffTemp : {e.DiffTemp}");
+        }
+        public void Subscribe(Thermostate thermostate)
+        {
+            thermostate.OnTemperatureChanged += ShowGif;
+        }
+        private void ShowGif(object sender , Thermostate.ChangeTemperatureEventArgs e)
+        {
+            pictureBox1.Visible = true;
+        }
+    }
+}
---
+++ Problem1/Thermostate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course20.TermostateProject
+{
+    public partial class Thermostate : UserControl
+    {
+        
+        public class ChangeTemperatureEventArgs : EventArgs
+        {
+            public decimal OldTemp { get; }
+            public decimal NewTemp { get; }
+            public decimal DiffTemp { get; }
+            public ChangeTemperatureEventArgs(decimal oldTemp , decimal newTemp , decimal diffTemp)
+            {
+                OldTemp = oldTemp;
+                NewTemp= newTemp;
+                DiffTemp = diffTemp;
+            }
+        }
+        public event EventHandler<ChangeTemperatureEventArgs> OnTemperatureChanged;
+        public Thermostate()
+        {
+            InitializeComponent();
+        }
+
+        private static int Counter=0;
+        private bool IsTempChanged(decimal Temp1 , decimal Temp2)
+        {
+            return Temp1 != Temp2;
+        }
+        private struct Temps
+        {
+            public decimal Temp1;
+            public decimal Temp2;
+            public decimal TemproryTemperature;
+        }
+        private Temps temps;
+        private void btnChangeTemp_Click(object sender, EventArgs e)
+        {
+        
+            if (txtbNewTemp.Text==null || txtbNewTemp.Text==string.Empty)
+            {
+                MessageBox.Show($"Please enter a tempreature in the text box...");
+            }
+            decimal OldTemp = decimal.Parse(lblOldTemp.Text);
+            decimal NewTemp = decimal.Parse(txtbNewTemp.Text);
+            if (!IsTempChanged(OldTemp, NewTemp))
+            {
+                MessageBox.Show($"Tempreature didn't change...");
+                return;
+            }
+           
+                lblOldTemp.Text = txtbNewTemp.Text;
+
+
+           
+            decimal DiffTemp = NewTemp- OldTemp;
+            OnTemperatureChanged(this,new ChangeTemperatureEventArgs(OldTemp,NewTemp,DiffTemp));
+        }
+    }
+}
